87ea21da419b5b62ca3af83a107c742a0d2f64dd
nsoiffer <soiffer@alum.mit.edu>
2026-04-28 10:29:54 -0700

    missed a spot where copy_mathml should be copy_mathml_recursive

--- src/interface.rs
+++ src/interface.rs
@@ -680,7 +680,7 @@ pub fn copy_mathml(mathml: Element) -> Element {
     let mut new_children = Vec::with_capacity(children.len());
     for child in children {
         let child = as_element(child);
-        let new_child = copy_mathml(child);
+        let new_child = copy_mathml_recursive(child, depth + 1);
         new_children.push(new_child);
     }
     new_mathml.append_children(new_children);
